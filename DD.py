8c2ca6f53b737a9645f20e11e6fcfdb2983ffc8a
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2023-01-17 14:14:59 -0700

    Update DD.py

--- DD.py
+++ DD.py
@@ -18,38 +18,38 @@ while Loop:
         print(str(DR)+", " + str(DR2))
         print("sum: " + str(DR+DR2))
 
-    #5 roll
+    # 5 roll
     elif menu == "2":
         for I in range(5):
-            FR = random.randrange(1,6)
-            FR2 = random.randrange(1,6)
+            FR = random.randrange(1, 6)
+            FR2 = random.randrange(1, 6)
             print("")
-            print( str(FR)+ ","+ str(FR2))
-            print("sum: "+ str(FR+FR2))
-    
-      #not working
+            print(str(FR) + "," + str(FR2))
+            print("sum: " + str(FR+FR2))
+
+      # not working
       # X roll
     elif menu == "3":
         num = int(input("choose a number: "))
         for I in range(num):
-            XR = random.randrange(1,6)
-            XR2 = random.randrange(1,6)
+            XR = random.randrange(1, 6)
+            XR2 = random.randrange(1, 6)
             print("")
-            print( str(XR)+ ","+ str(XR2))
-            print("sum: "+ str(XR+XR2))
+            print(str(XR) + "," + str(XR2))
+            print("sum: " + str(XR+XR2))
     # SE roll
     elif menu == "4":
         loop = True
         while loop:
-            SER = random.randrange(1,6)
-            SER2 = random.randrange(1,6)
+            SER = random.randrange(1, 6)
+            SER2 = random.randrange(1, 6)
             print("")
-            print( str(SER)+ ","+ str(SER2))
-            print("sum: "+ str(SER+SER2))
+            print(str(SER) + "," + str(SER2))
+            print("sum: " + str(SER+SER2))
             if SER+SER2 == 2:
-                print ("Snake Eyes")
-                loop=False
-    # setup before eveything else above, 
+                print("Snake Eyes")
+                loop = False
+    # setup before eveything else above,
     # exit
     if menu == "5":
         Loop = False
